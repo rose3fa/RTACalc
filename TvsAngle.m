76a338f92c69a02578442b5a6904bfae36c92de6
rose3fa <44813441+rose3fa@users.noreply.github.com>
2018-11-06 10:41:12 -0700

    Delete TvsAngle.m

--- TvsAngle.m
+++
@@ -1,7 +0,0 @@
-function [T, n_substrate]=TvsAngle(lam0, lam1, dlam, layers, thicknesses, angle, polarization, wl)
-% Preallocate memory
-T = zeros(length(wl), length(angle));    
-% Calculate A for every angle
-for q = 1:length(angle) 
-    [~, n_substrate,~,T(:,q),~, ~, ~, ~, ~, ~]=thinfilmRTA(lam0, lam1, dlam, layers, thicknesses, angle(q), polarization);
-end
