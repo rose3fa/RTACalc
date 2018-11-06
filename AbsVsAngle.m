88699b52114569f469ea7b021b173222943a3724
rose3fa <44813441+rose3fa@users.noreply.github.com>
2018-11-06 10:38:43 -0700

    Delete AbsVsAngle.m

--- AbsVsAngle.m
+++
@@ -1,8 +0,0 @@
-function [Abs, n_substrate]=AbsVsAngle(lam0, lam1, dlam, layers, thicknesses, angle, polarization, wl)
-% Preallocate memory
-T = zeros(length(wl), length(angle));    
-% Calculate A for every angle
-for q = 1:length(angle) 
-    [~, n_substrate,~,T(:,q),~, ~, ~, ~, ~, ~]=thinfilmRTA(lam0, lam1, dlam, layers, thicknesses, angle(q), polarization);
-end
-Abs = -log10(T);
